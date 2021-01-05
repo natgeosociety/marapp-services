73bbd3304f7fc86efb946f46105b9bad937779be
Horia Iacos <iacos.horia@gmail.com>
2021-01-05 14:29:40 +0200

    chore: cleanup

--- support/bootstrap-fixture-data.ts
+++ support/bootstrap-fixture-data.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env ts-node --files
+#!/usr/bin/env ./node_modules/.bin/ts-node --files
 
 /*
   Copyright 2018-2020 National Geographic Society
@@ -20,44 +20,26 @@
 */
 
 import axios from 'axios';
+import { has, isString, merge, set } from 'lodash';
 import * as readline from 'readline';
-import slug from 'slug';
-import { v4 as uuidv4 } from 'uuid';
 import * as yargs from 'yargs';
 
 import { API_BASE } from '../src/config';
-import { Location, LocationTypeEnum, Layer, LayerTypeEnum, LayerCategoryEnum } from '../src/models';
+import { Layer, LayerCategoryEnum, LayerProviderEnum, LayerTypeEnum, Location, LocationTypeEnum } from '../src/models';
 
 const argv = yargs.options({
   apiHost: { type: 'string', demandOption: true },
   organization: { type: 'string', demandOption: true },
   dryRun: { type: 'boolean', default: false },
   apiKey: { type: 'string', demandOption: true },
-  type: { type: 'string', default: 'location' },
+  type: { type: 'string', choices: ['location', 'layer'], default: 'location' },
 }).argv;
-
-const cleanStr = (str: string) => {
-  return str ? str.trim() : null;
-};
-
-const slugifyName = (str: string) => {
-  const sanitized = cleanStr(str);
-
-  return slug(sanitized).toLowerCase();
-};
-
-const splitPascalCase = (str: string) => {
-  return cleanStr(str)
-    .replace(/([A-Z][a-z])/g, ' $1')
-    .replace(/(\d)/g, ' $1')
-    .trim();
-};
 
 interface IResourceCreator {
   processLine?(line: string): Promise<void>;
   processData?(data: any, parent: IResourceCreator): Promise<void>;
-  writeHeaders?(): void;
-  toStdout(record: any, error?: boolean);
+  writeHeaders?(line: string): void;
+  toStdout(record: any, error?: boolean, sep?: string);
   createResource(body: any, endpoint?: string): Promise<void>;
 }
 
@@ -68,17 +50,26 @@ abstract class ResourceCreator implements IResourceCreator {
       } else if (argv.apiKey) {
         await parent.createResource(data);
       }
-
       parent.toStdout(data);
     } catch (err) {
       parent.toStdout(data, true);
     }
   }
 
-  toStdout(record: any, error: boolean = false) {
-    const print = error ? console.error.bind(console) : console.debug.bind(console);
+  writeHeaders(line: string, sep: string = '\t'): void {
+    const data = JSON.parse(line);
+    console.log(Object.keys(data).sort().join(sep));
+  }
 
-    print(record);
+  toStdout(record: any, error: boolean = false, sep: string = '\t') {
+    const keys = Object.keys(record).sort();
+    const message = keys.reduce((acc, key) => {
+      acc.push(record[key]);
+      return acc;
+    }, []);
+
+    const print = error ? console.error.bind(console) : console.debug.bind(console);
+    print(message.join(sep));
   }
 
   async createResource(body: any, endpoint: string): Promise<void> {
@@ -86,51 +77,60 @@ abstract class ResourceCreator implements IResourceCreator {
       ApiKey: argv.apiKey,
       'Content-Type': 'application/json',
     };
-
     try {
       const response = await axios.post(endpoint, body, { headers });
-
       if (response.status === 200) {
         return response.data;
       }
     } catch (err) {
       console.error(err.message);
-
       if (err.response && err.response.data) {
         console.error(JSON.stringify(err.response.data, null, 2));
       }
     }
   }
+
+  /**
+   * Validate optional fields if present in the input.
+   * @param data
+   * @param optionals
+   */
+  parseOptionals = <T, L extends keyof T>(data: T, optionals: L[] = []): any => {
+    const output = {};
+    optionals.forEach((key) => {
+      if (has(data, key)) {
+        const value = data[key];
+        set(output, key, isString(value) ? this.cleanStr(<any>value) : value);
+      }
+    });
+    return output;
+  };
+
+  cleanStr = (str: string) => {
+    return str ? str.trim() : null;
+  };
 }
 
 class LocationResourceCreator extends ResourceCreator {
   async processLine(line: string): Promise<void> {
-    const data = JSON.parse(line);
+    const data: Partial<Location> = JSON.parse(line);
 
-    const location: Location = {
-      id: data['id'] || uuidv4(),
-      slug: slugifyName(data['name']),
-      name: cleanStr(data['name']),
-      type: <LocationTypeEnum>splitPascalCase(data['type']),
-      description: cleanStr(data['description']),
-      published: data['published'],
-      featured: data['featured'],
-      geojson: data['geojson'],
-      publicResource: !!data['publicResource'],
-      organization: argv.organization,
+    const mandatory: Partial<Location> = {
+      name: this.cleanStr(data.name),
+      type: <LocationTypeEnum>this.cleanStr(data.type),
+      geojson: data.geojson,
     };
+    const optional = this.parseOptionals(data, [
+      'id',
+      'slug',
+      'description',
+      'published',
+      'featured',
+      'publicResource',
+    ]);
+    const location = merge(mandatory, optional);
 
     await super.processData(location, this);
-  }
-
-  writeHeaders(): void {
-    console.log(['<ID>', '<SLUG>', '<NAME>', '<TYPE>', '<FEATURED>', '<PUBLISHED>'].join('\t'));
-  }
-
-  toStdout(record: Location, error: boolean = false) {
-    const message = [record.id, record.slug, record.name, record.type, record.featured, record.published].join('\t');
-
-    super.toStdout(message, error);
   }
 
   async createResource(body: Location): Promise<void> {
@@ -140,59 +140,19 @@ class LocationResourceCreator extends ResourceCreator {
 
 class LayerResourceCreator extends ResourceCreator {
   async processLine(line: string): Promise<void> {
-    const data = JSON.parse(line);
+    const data: Partial<Layer> = JSON.parse(line);
 
-    const layer: Layer = {
-      id: data['id'] || uuidv4(),
-      slug: slugifyName(data['name']),
-      name: cleanStr(data['name']),
-      type: <LayerTypeEnum>splitPascalCase(data['type']),
-      category: [<LayerCategoryEnum>data['category']],
-      description: cleanStr(data['description']),
-      published: String(data['published']).toLowerCase() === 'true',
-      primary: String(data['primary']).toLowerCase() === 'true',
-      provider: data['provider'],
-      config: JSON.parse(data['config']),
-      references: data['references'] || [],
-      organization: argv.organization,
+    const mandatory: Partial<Layer> = {
+      name: this.cleanStr(data.name),
+      type: <LayerTypeEnum>this.cleanStr(data.type),
+      category: [<LayerCategoryEnum>this.cleanStr(<any>data.category)],
+      provider: <LayerProviderEnum>this.cleanStr(data.provider),
+      config: isString(data.config) ? JSON.parse(data.config) : data.config,
     };
+    const optional = this.parseOptionals(data, ['id', 'slug', 'description', 'published', 'primary', 'references']);
+    const layer = merge(mandatory, optional);
 
     await super.processData(layer, this);
-  }
-
-  writeHeaders(): void {
-    console.log(
-      [
-        '<ID>',
-        '<SLUG>',
-        '<NAME>',
-        '<TYPE>',
-        '<CATEGORY>',
-        '<DESCRIPTION>',
-        '<PUBLISHED>',
-        '<PRIMARY>',
-        '<PROVIDER>',
-        '<CONFIG>',
-        '<REFERENCES>',
-      ].join('\t')
-    );
-  }
-
-  toStdout(record: Layer, error: boolean = false) {
-    const message = [
-      record.id,
-      record.slug,
-      record.name,
-      record.type,
-      record.category,
-      record.description,
-      record.published,
-      record.primary,
-      record.provider,
-      record.config,
-    ].join('\t');
-
-    super.toStdout(message, error);
   }
 
   async createResource(body: Layer): Promise<void> {
@@ -206,9 +166,10 @@ const main = async (): Promise<void> => {
   switch (argv.type) {
     case 'location':
       resourceCreator = new LocationResourceCreator();
-
+      break;
     case 'layer':
       resourceCreator = new LayerResourceCreator();
+      break;
   }
 
   const reader = readline.createInterface({
@@ -219,9 +180,8 @@ const main = async (): Promise<void> => {
 
   for await (const line of reader) {
     if (counter === 0) {
-      resourceCreator.writeHeaders();
+      resourceCreator.writeHeaders(line);
     }
-
     counter++;
 
     await resourceCreator.processLine(line);
